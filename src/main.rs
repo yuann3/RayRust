7983b488bc23f615215d40ef3fe6ccc6be228e4d
Yiyuan Li <yy@eyuan.me>
2024-11-06 19:13:57 +0800

    hello world?

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    // Image
+    let image_width = 256;
+    let image_height = 256;
+
+    // PPM header
+    println!("P3");
+    println!("{} {}", image_width, image_height);
+    println!("255");
+
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let r = i as f64 / (image_width - 1) as f64;
+            let g = j as f64 / (image_height - 1) as f64;
+            let b = 0.0;
+
+            let ir = (255.99 * r) as i32;
+            let ig = (255.99 * g) as i32;
+            let ib = (255.99 * b) as i32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+}
