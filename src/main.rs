604e42319763f07934a7ceea11140e672f895fe9
Yiyuan Li <yy@eyuan.me>
2024-11-11 18:03:05 +0800

    add indicator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 fn main() {
     // Image
     let image_width = 256;
@@ -9,11 +11,16 @@ fn main() {
     println!("255");
 
     for j in 0..image_height {
+        // Progress indicator
+        eprint!("\r Scanliens: {}", image_height - j);
+        io::stderr().flush().unwrap(); // make sure immediate output
+
         for i in 0..image_width {
             let r = i as f64 / (image_width - 1) as f64;
             let g = j as f64 / (image_height - 1) as f64;
             let b = 0.0;
 
+            // Convert to integer values;
             let ir = (255.99 * r) as i32;
             let ig = (255.99 * g) as i32;
             let ib = (255.99 * b) as i32;
@@ -21,4 +28,5 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+    eprintln!("\rDone.                 ");
 }
