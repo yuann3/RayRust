78a138493b78ed2060479aa0dada1de8fd78849d
Yiyuan Li <yy@eyuan.me>
2024-11-11 18:03:48 +0800

    vec3 Class
    implementing operator overloading

---
+++ src/vec3.rs
@@ -0,0 +1,101 @@
+use std::ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Sub, SubAssign};
+
+#[derive(Debug, Clone, Copy)]
+pub struct Vec3 {
+    e: [f64; 3],
+}
+
+impl Vec3 {
+    pub fn new(e0: f64, e1: f64, e2: f64) -> Vec3 {
+        Vec3 { e: [e0, e1, e2] }
+    }
+
+    pub fn zero() -> Vec3 {
+        Vec3 { e: [0.0, 0.0, 0.0] }
+    }
+
+    // Getters
+    pub fn x(&self) -> f64 { self.e[0] }
+    pub fn y(&self) -> f64 { self.e[1] }
+    pub fn z(&self) -> f64 { self.e[2] }
+
+    // Vector length
+    pub fn length(&self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn length_squared(&self) -> f64 {
+        self.e[0] * self.e[0] +
+        self.e[1] * self.e[1] +
+        self.e[2] * self.e[2]
+    }
+
+    // Utility methods
+    pub fn dot(&self, other: &Vec3) -> f64 {
+        self.e[0] * other.e[0] + 
+        self.e[1] * other.e[1] + 
+        self.e[2] * other.e[2]
+    }
+
+    pub fn cross(&self, other: &Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[1] * other.e[2] - self.e[2] * other.e[1],
+                self.e[2] * other.e[0] - self.e[0] * other.e[2],
+                self.e[0] * other.e[1] - self.e[1] * other.e[0],
+            ],
+        }
+    }
+
+    pub fn unit_vector(&self) -> Vec3 {
+        *self / self.length()
+    }
+}
+
+pub type Point3 = Vec3;
+pub type Color = Vec3;
+
+/// Operator Over loading
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] + other.e[0],
+                self.e[1] + other.e[1],
+                self.e[2] + other.e[2],
+            ]
+        }
+    }
+}
+
+impl AddAssign for Vec3 {
+    fn add_assign(&mut self, other: Self) {
+        self.e[0] += other.e[0];
+        self.e[1] += other.e[1];
+        self.e[2] += other.e[2];
+    }
+}
+
+impl Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, other: Vec3) -> Vec3 {
+        Vec3 {
+            e: [
+                self.e[0] - other.e[0],
+                self.e[1] - other.e[1],
+                self.e[2] - other.e[2],
+            ]
+        }
+    }
+}
+
+impl SubAssign for Vec3 {
+    fn sub_assign (&mut self, other: Self) {
+        self.e[0] -= other.e[0];
+        self.e[1] -= other.e[1];
+        self.e[2] -= other.e[2];
+    }
+}
